026e743bf6dc465fd4945942cf3f65e836f00085
Nele Hornbostel <nele.hornbostel@sap.com>
2026-03-23 14:25:28 +0100

    first draft feature overview on landing page

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import DocsCards from './components/DocsCards.vue'
 import DrawioDiagram from './components/DrawioDiagram.vue'
 import FeatureOverview from './components/FeatureOverview.vue'
 import DrawioDiagram from './components/DrawioDiagram.vue'
+import FeatureOverview from './components/FeatureOverview.vue'
 import './custom.css'
 
 export default {
@@ -14,5 +15,6 @@ export default {
     app.component('DrawioDiagram', DrawioDiagram)
     app.component('FeatureOverview', FeatureOverview)
     app.component('DrawioDiagram', DrawioDiagram)
+    app.component('FeatureOverview', FeatureOverview)
   }
 }
